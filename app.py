64cd5dda506b2a33317094dfd17b3635f5ff8582
infinitycoders-046 <infinitycoders@zohomail.in>
2026-06-23 14:47:21 +0530

    commit again

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/dashboard01")
+def dashboard01():
+    return render_template("dashboard01.html")
+
+@app.route("/dashboard02")
+def dashboard02():
+    return render_template("dashboard02.html")
+
+@app.route("/dashboard03")
+def dashboard03():
+    return render_template("dashboard03.html")
+
+@app.route("/story")
+def story():
+    return render_template("story.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
